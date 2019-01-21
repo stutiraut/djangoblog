9b3612e0bf6ba2eff9bb7280dab394e8348fce2f
stutiraut <sraut@unomaha.edu>
2019-01-21 15:53:44 -0600

    Added taggit, comments and similar post functionality

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -30,6 +31,7 @@ class Post(models.Model):
 
     objects = models.Manager()  # The default manager.
     published = PublishedManager()  # Our custom manager.
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
@@ -44,3 +46,20 @@ class Post(models.Model):
                              self.publish.day,
                              self.slug])
 
+class Comment(models.Model):
+        post = models.ForeignKey(Post,
+                                 on_delete=models.CASCADE,
+                                 related_name='comments')
+        name = models.CharField(max_length=80)
+        email = models.EmailField()
+        body = models.TextField()
+        created = models.DateTimeField(auto_now_add=True)
+        updated = models.DateTimeField(auto_now=True)
+        active = models.BooleanField(default=True)
+
+        class Meta:
+            ordering = ('created',)
+
+        def __str__(self):
+
+             return 'Comment by {} on {}'.format(self.name, self.post)
